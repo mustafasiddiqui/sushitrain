ad1f59c3faf436273c995e3caae47d1558cdf1a6
Mustafa Siddiqui <mustafasiddiqui79@gmail.com>
2023-06-06 18:15:14 -0500

    Rename SushiTrainApplication

--- src/main/java/com/example/sushitrain/SushitrainApplication.java
+++ src/main/java/com/example/sushitrain/SushitrainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SushitrainApplication {
+public class SushiTrainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SushitrainApplication.class, args);
+		SpringApplication.run(SushiTrainApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/sushitrain/SushitrainApplicationTests.java
+++ src/test/java/com/example/sushitrain/SushitrainApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SushitrainApplicationTests {
+class SushiTrainApplicationTests {
 
 	@Test
 	void contextLoads() {
