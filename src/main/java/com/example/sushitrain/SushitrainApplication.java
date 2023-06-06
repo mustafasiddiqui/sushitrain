ea64ea8a2944036f83b20d8a86cb8e0f5df89e13
Mustafa Siddiqui <mustafasiddiqui79@gmail.com>
2023-06-06 18:11:14 -0500

    Initial commit

---
+++ src/main/java/com/example/sushitrain/SushitrainApplication.java
@@ -0,0 +1,13 @@
+package com.example.sushitrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushitrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushitrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sushitrain/SushitrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sushitrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushitrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
