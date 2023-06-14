ab8cd44fcc310c9495e86dca38ece5583a7f0459
Mustafa Siddiqui <mustafasiddiqui79@gmail.com>
2023-06-14 17:03:42 -0500

    form: Add front end for uploading and returning results

--- src/main/java/com/example/sushitrain/controller/FileUploadController.java
+++ src/main/java/com/example/sushitrain/controller/FileUploadController.java
@@ -2,11 +2,14 @@ package com.example.sushitrain.controller;
 
 import com.example.sushitrain.scanner.TrayScannerImpl;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -15,17 +18,30 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@RestController
+@Controller
 public class FileUploadController {
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/uploadFile")
+    public String listUploadedFiles(Model model) {
+        return "uploadForm";
+    }
+
     @PostMapping("/uploadFile")
-    public int handleFileUpload(@RequestParam MultipartFile file) {
+    public String handleFileUpload(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
         try {
             String uploadedFilePath = convertMultipartFileToFile(file);
             TrayScannerImpl scanner = new TrayScannerImpl();
             int retVal = scanner.scanTrays(uploadedFilePath);
             deleteFile(uploadedFilePath);
-            return retVal;
+            redirectAttributes
+                    .addFlashAttribute("result", retVal)
+                    .addFlashAttribute("successfulUpload", true);
+            return "redirect:/";
         } catch (IOException e) {
             e.printStackTrace();
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Error when uploading file");
